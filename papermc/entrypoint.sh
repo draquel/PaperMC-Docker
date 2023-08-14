7fe251990d1160a61f321f5fdee32b0f9541b906
Dan Raquel <draquel@webjynx.com>
2023-08-14 04:52:05 +0000

    updated docker file and entry point to improve handling of mysql database creation and removed attempts to cache the rcon password

--- papermc/entrypoint.sh
+++ papermc/entrypoint.sh
@@ -5,6 +5,23 @@ set -e
 DOCKER_USER='dockeruser'
 DOCKER_GROUP='dockergroup'
 
+if test -f "/app/server.properties"; then
+  cp -f /app/server.properties /data/server.properties
+fi
+
+if test -d "/app/plugins"; then
+  if test ! -d "/data/plugins"; then
+    mkdir "/data/plugins"
+  fi
+  cp -f -r /app/plugins/* /data/plugins/
+  grep -rl %%MC-WORLD-DB%% /data/plugins | xargs -r sed -i "s/%%MC-WORLD-DB%%/$DATABASE_NAME/g"
+fi
+
+if test -d "/app/config"; then
+  cp -f -r /app/config /data/config
+  #grep -rl %%MC-WORLD-DB%% /data/config | xargs sed -i "s/%%MC-WORLD-DB%%/$DATABASE_NAME/g"
+fi
+
 if ! id "$DOCKER_USER" >/dev/null 2>&1; then
     echo "First start of the docker container, start initialization process."
 
@@ -26,3 +43,4 @@ fi
 
 export HOME=/home/$DOCKER_USER
 exec gosu $DOCKER_USER:$DOCKER_GROUP java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /app/minecraft/papermc.jar $PAPERMC_FLAGS --nogui --universe universe
+
